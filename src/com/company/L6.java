2183e6d7e92d608ba48e23d43488f5dbc620b529
admin <1406h1mb33r3>
2015-08-06 20:17:37 +0300

    class 1

---
+++ src/com/company/L6.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by admin on 06.08.15.
+ */
+public class L6 {
+
+    public static void main() {
+
+            int[] massiv = {1, 10, -20, 30};
+            for (int abc : massiv) {
+                System.out.println(abc);
+            }
+
+    }
+
+
+}
+
+
