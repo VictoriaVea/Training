32539ac4260393aad5db30acd8c4913909adfd64
VictoriaVea <v.nizhelska@gmail.com>
2015-08-24 18:56:41 +0300

    Home6_2 commit home tasks

--- src/com/company/Home6_2.java
+++ src/com/company/Home6_2.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.lang.model.element.Name;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,42 +33,47 @@ public class Home6_2 {
         // Test PASSED.
         test("Hello to all 14 students!", hello(14));
         // Test PASSED.
+        System.out.println("\n");
         test("Hello student!", hello(1));
         // Test PASSED.
         test("No one came.", hello(0));
         // Test PASSED.
         test("Hello to all 25 persons!", hello(25, "persons"));
         // Test PASSED.
+        System.out.println("\n");
         test("Hello to all 99 programmers!", hello(99, "programmers"));
         // Test PASSED.
-        test("Hello Jean Podmore Oleksiia !", hello(new String[] {"Jean", "Podmore", "Oleksiia"}));
+        test("Hello Jean Podmore Oleksiia !", hello(new String[]{"Jean", "Podmore", "Oleksiia"}));
         // Test PASSED.
         test("Hello Anonymous Anonymous2 !", hello(new String[] {"Anonymous", "Anonymous2"}));
         // Test PASSED.
+        System.out.println("\n");
         test("Hello Slughorn, Norris, Fred and George!", hello(Arrays.asList("Slughorn", "Norris", "Fred", "George")));
         // Test PASSED.
         test("Hello Horace, Miss and Weasley's!", hello(Arrays.asList("Horace", "Miss", "Weasley's")));
         // Test PASSED.
         test(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
         // Test PASSED.
+        System.out.println("\n");
         test(Arrays.asList(1, 4, 3), Arrays.asList(1, 2, 3));
         // Test FAILED.
         // Expected: [1, 4, 3]
         // Actual:   [1, 2, 3]
-        //test(Arrays.asList(6), stringsLength(Arrays.asList("Jordan")));
+
+        test(Arrays.asList(6), stringsLength(Arrays.asList("Jordan")));
         // Test PASSED.
-        //test(Arrays.asList(5, 3, 8), stringsLength(Arrays.asList("Harry", "Ron", "Hermione")));
+        test(Arrays.asList(5, 3, 8), stringsLength(Arrays.asList("Harry", "Ron", "Hermione")));
         // Test PASSED.
-        //test("[5, 5]", stringsLength(Arrays.asList("Jinny", "Lupin")).toString());
+        test("[5, 5]", stringsLength(Arrays.asList("Jinny", "Lupin")).toString());
         // Test PASSED.
-        //test(Arrays.asList(3, 6, 1, -60), reverseInts(Arrays.asList(-60, 1, 6, 3)));
+        test(Arrays.asList(3, 6, 1, -60), reverseInts(Arrays.asList(-60, 1, 6, 3)));
         // Test PASSED.
-        //prettyPrint(Arrays.asList("Vernon", "Dudley", "Petunia", "Madge"), true);
+        prettyPrint(Arrays.asList("Vernon", "Dudley", "Petunia", "Madge"), true);
         // Vernon
         //  Dudley
         //   Petunia
         //    Madge
-        //prettyPrint(reverse(Arrays.asList("Vernon", "Dudley", "Petunia", "Madge")), false);
+        prettyPrint(reverse(Arrays.asList("Vernon", "Dudley", "Petunia", "Madge")), false);
         //    Madge
         //   Petunia
         //  Dudley
@@ -109,7 +115,7 @@ public class Home6_2 {
     public static String hello (String[] listed) {
         String Output = "Hello ";
         for (String i : listed) {
-            Output = Output + i + " ";
+            Output += i + " ";
         }
         return Output+ "!";
     }
@@ -120,32 +126,89 @@ public class Home6_2 {
 
     public static String hello(List <String> nameList) {
         String nameset="Hello ";
-        for (int i=0;i<nameList.size()-2;i++){
-            nameset=nameset+nameList.get(i)+", ";
+        for (int i=0;i<nameList.size();i++){
+            if (i==nameList.size()-2){
+                nameset+=nameList.get(i)+" and "+nameList.get(i+1)+"!";
+                break;
+            }
+            nameset+=nameList.get(i)+", ";
         }
-        nameset=nameset+nameList.get(nameList.size()-1)+" and "+nameList.get(nameList.size());
-        return nameset+"!";
+        return nameset;
     }
 
     public static void test(List <Integer> L1, List <Integer> L2) {
+        boolean res=true;
         if (L1.size() != L2.size()) {
-            System.out.println(L1.size()+","+L2.size());
-            System.out.println("Test FAILED.");
-        } else if (L1.size() == L2.size()) {
+            res=false;
+        } else {
             if (L1.size() == 0) {
-                System.out.println("Test PASSED.");
+                res=true;
             } else {
                 for (int i = 0; i < L1.size(); i++) {
-                    if (Objects.equals(L1.get(i), L2.get(i))) {
-                        System.out.println("Test PASSED.");
-                        System.out.println(L1.get(i));
+                    if (L1.get(i)==L2.get(i)) {
+                        res=true;
                     } else {
-                        System.out.println("Test FAILED."+"/n"+"Expected: "+L1+"/n"+"Actual:   "+L2);
+                        res=false;
                         break;
                     }
                 }
             }
         }
+        if (res){
+            System.out.println("Test PASSED.");
+        }else {
+            System.out.println("Test FAILED." + "\n" + "Expected: " + L1 + "\n" + "Actual:   " + L2);
+        }
     }
+
+    public static List<Integer> stringsLength(List <String> NameFull) {
+        ArrayList<Integer> output = new ArrayList<>();
+        for (String Word:NameFull){
+            output.add(Word.length());
+        }
+        return output;
+    }
+
+    public static void prettyPrint (List<String> NameSet, boolean way){
+        int space=0;
+        int spacemax = NameSet.size()-1;
+        if (way){
+            for (String n:NameSet) {
+                System.out.println(repeat(" ",space)+n);
+                space++;
+            }
+        }else{
+            for (String n:NameSet) {
+                System.out.println(repeat(" ",spacemax)+n);
+                spacemax--;
+            }
+        }
+    }
+
+    public static String repeat (String s,int num){
+        String res="";
+        for (int i=0;i<num;i++){
+            res+=s;
+        }
+        return res;
+    }
+
+    public static List<String> reverse(List <String> NameSet){
+        ArrayList <String> set=new ArrayList<>();
+        for (int i=NameSet.size()-1;i>=0;i--){
+            set.add(NameSet.get(i));
+        }
+        return set;
+    }
+
+    public static List<Integer> reverseInts(List <Integer> row){
+        ArrayList <Integer> set=new ArrayList<>();
+        for (int i=row.size()-1;i>=0;i--){
+            set.add(row.get(i));
+        }
+        return set;
+    }
+
 }
+
 
